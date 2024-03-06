7a86598efeadc5b51c1105933f0ebc0ee33c22d3
ThamiBa <baladithami@gmail.com>
2024-03-06 02:09:01 +0100

    backend-js

---
+++ 0x00-ES6_basic/11-main.js
@@ -0,0 +1,4 @@
+import createEmployeesObject from './11-createEmployeesObject.js';
+
+console.log(createEmployeesObject("Software", [ "Bob", "Sylvie" ]));
+console.log(createEmployeesObject("Winners", [ "Meriem", "Thami" ]));
