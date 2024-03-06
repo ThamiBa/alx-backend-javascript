64467c263dbaf3ae4bbcaf424e934a2cf5ac236e
ThamiBa <baladithami@gmail.com>
2024-03-06 01:31:11 +0100

    backend-js

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,1 +1,15 @@
-#!/usr/bin/node
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+// alx-backend-javascript/0x00-ES6_basic/8-getBudgetCurrentYear.js
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+
+  return budget;
+}
