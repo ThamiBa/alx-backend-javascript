9ae38d9598dcc1575c6264b4748ea3e958688ac5
ThamiBa <baladithami@gmail.com>
2024-03-08 02:54:23 +0100

    backend-js-classes

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -4,10 +4,6 @@ export default class Airport {
     this._code = code;
   }
 
-  toString() {
-    return `${this._name} (${this._code})`;
-  }
-
   get [Symbol.toStringTag]() {
     return this._code;
   }
